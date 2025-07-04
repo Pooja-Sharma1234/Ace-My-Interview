91891d5a305929e88c88432a49eea49685011d5b
pooja <pooja8295sharma@gmail.com>
2025-07-04 12:12:02 +0530

    db change

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,19 +9,3 @@ const connectDB = async () => {
   }
 };
 module.exports = connectDB;
-
-///
-
-// jayse jab forntend se data bhjatae hian
-
-// const data = {
-//   name: "tj",
-// };
-
-// JSON.stringify(data); //yeah server ko bhajte hian is format main "{"name":"tj"}" //ayse bjata server
-//phir server isse json main convert kr deta hian phir server isse dubrat wapis original form kr leta hian
-// {
-//   name: "tj",
-// };
-//samjh aya kya tj dikhne m to same hih hahahaa kya yeah kya haese kese phele string hian laga ok ok vse server ko ky ese chahiye hota h sidha sidha ku ni leta
-//acha sidha kuch hian kya jo server bhi sidha len are logically puch rhi hu are nahi tj string ko modify nahi kiya ja sakta  mtlb jab data FE se BE ata hian aur bhi bahut se tarike hian data ko bhajne ka abhi maan ligiye ayse hi kaam krta hain ok v
